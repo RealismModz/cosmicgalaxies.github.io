298b9743c2d47c06d40d470dfa2e8e27e8a6dad7
Adam <74314445+RealismModz@users.noreply.github.com>
2021-01-23 14:27:39 -0600

    Add files via upload

---
+++ js/noclick.js
@@ -0,0 +1,4 @@
+document.getElementById("bg").oncontextmenu = new Function("return false;");
+document.getElementById("Realism").oncontextmenu = new Function("return false;");
+document.getElementById("TK").oncontextmenu = new Function("return false;");
+document.getElementById("Name").oncontextmenu = new Function("return false;");
